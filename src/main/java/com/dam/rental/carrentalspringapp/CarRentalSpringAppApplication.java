81cb489cd039b32c331aad2caf7a0f86c5fb8b9e
alexjeq <alexjechiumd@gmail.com>
2022-11-09 00:44:55 +0200

    First commit

---
+++ src/main/java/com/dam/rental/carrentalspringapp/CarRentalSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.dam.rental.carrentalspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/rental/carrentalspringapp/CarRentalSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.rental.carrentalspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
